a59e0261c194d258dd87b3b8c05244b5ff8f5875
Justin Acheaibi <justin.acheaibi@gmail.com>
2025-03-21 11:29:56 +0100

    modification présentation et ajout des projets

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,19 +1,16 @@
 import React from "react";
 import "@styles/footer.scss";
-import { FaFacebook, FaTwitter, FaLinkedin } from "react-icons/fa";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
 
 const Footer = () => {
   return (
     <footer>
       <p>© 2024 Mon Portfolio - Tous droits réservés</p>
       <div className="social-icons">
-        <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">
-          <FaFacebook />
+        <a href="https://github.com/Justin-oc" alt="Repo GitHub" target="_blank" rel="noopener">
+          <FaGithub />
         </a>
-        <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">
-          <FaTwitter />
-        </a>
-        <a href="https://linkedin.com" target="_blank" rel="noopener noreferrer">
+        <a href="https://www.linkedin.com/in/justin-acheaibi-585687300/" alt="Profil LinkedIn" target="_blank" rel="noopener">
           <FaLinkedin />
         </a>
       </div>
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 import "@styles/about.scss";
+import { FaReact, FaUniversalAccess } from "react-icons/fa";
+import { SiJavascript, SiHtml5, SiCss3, SiSass, SiBootstrap, SiGit, SiGithub } from "react-icons/si";
+import { MdSpeed } from "react-icons/md";
 
 const About = () => {
   return (
@@ -7,27 +10,28 @@ const About = () => {
       <div className="container">
         <h2>À propos de moi</h2>
         <p>
-          Je suis un développeur front-end passionné, je suis à l'aise en React et efficace dans
-          le développement de sites web moderne. J'aime créer des interfaces interactives,
-          performantes et accessibles.
+          Je suis un développeur front-end passionné, à l'aise en React et efficace dans
+          le développement de sites web moderne. Bien que je sois en début de parcours,
+          j'ai la soif d'apprendre et de me former jour après jour. J'aime créer des interfaces 
+          interactives, performantes et accessibles.
         </p>
 
         <div className="about-content">
-          <img src="/assets/profile.jpg" alt="Moi" className="profile-pic" />
+          <img src="./public/images/photo_justin.jpg" alt="photographie de Justin" className="profile-pic" />
           <div className="about-text">
             <h3>Mon parcours</h3>
             <p>
-            Fort d'une expérience en aide-soignant qui m'a permis de développer un excellent relationnel,
-            une grande adaptabilité et une soif constante d'apprendre, j'ai relevé le défi d'une reconversio
+            Fort d'une expérience en tant qu'aide-soignant qui m'a permis de développer un excellent relationnel,
+            une grande adaptabilité et une soif constante d'apprendre, j'ai relevé le défi d'une reconversion
             en développement web, une passion concrétisée par ma formation chez OpenClassrooms. 
             Disponible dès avril 2025, je suis prêt à mettre mes compétences à votre service.
             </p>
             <h3>Mes compétences</h3>
             <ul>
-              <li>React, JavaScript, TypeScript</li>
-              <li>HTML, CSS, Sass, Bootstrap</li>
-              <li>Git, GitHub, CI/CD</li>
-              <li>Performance Web & Accessibilité</li>
+              <li>React, JavaScript <span className="tech-icons"><FaReact /><SiJavascript /></span></li>
+              <li>HTML, CSS, Sass, Bootstrap<span className="tech-icons"><SiHtml5 /><SiCss3 /><SiSass /><SiBootstrap /></span></li>
+              <li>Git, GitHub<span className="tech-icons"><SiGit /><SiGithub /></span></li>
+              <li>Performance Web & Accessibilité<span className="tech-icons"><MdSpeed /><FaUniversalAccess /></span></li>
             </ul>
           </div>
         </div>
